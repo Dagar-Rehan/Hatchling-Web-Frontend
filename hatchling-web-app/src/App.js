596aea11098263af7bb3aaacb186aaa158acd9dc
Dagar-Rehan <dagar.rehan@gmail.com>
2022-10-25 09:47:15 -0500

    [navbar][App] Working on navigation bar

---
+++ hatchling-web-app/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/navbar';
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+*/
+function App() {
+  return (<Navbar />);
+}
+
+export default App;
---
+++ hatchling-web-app/src/components/navbar.jsx
@@ -0,0 +1,25 @@
+import logo from './Hatchling.png';
+
+export default function Navbar(){
+    return (
+            <nav className="nav">
+                <img src={logo} alt="Logo" className="logo"/>
+                <a href="/" className="site-title" >Hatchling
+                </a>
+                <ul>
+                    <li>
+                        <a href="/Home" className="home">Home</a>
+                    </li>
+                    <li>
+                        <a href="/Characters" className="characters">Characters</a>
+                    </li>
+                    <li>
+                        <a href="/Help" className="help">Help</a>
+                    </li>
+                    <li>
+                        <a href="/About" className="about">About</a>
+                    </li>
+                </ul>
+            </nav>
+    );
+}
