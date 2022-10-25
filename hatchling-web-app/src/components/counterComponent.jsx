e1756c74333283e524bfd0ca858a1685fba8c612
Dagar-Rehan <dagar.rehan@gmail.com>
2022-10-25 09:47:15 -0500

    Working on navigation bar

---
+++ hatchling-web-app/src/components/counterComponent.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+class Counter extends Component {
+    state = {
+        count: 0
+      };
+
+    render() { 
+        return (
+            <React.Fragment>
+                <span>{this.formatCount()}</span>
+                <button>Increment</button>
+            </React.Fragment>
+            );
+    }
+
+    formatCount(){
+        return this.state.count === 0 ? <h1>Zero</h1> : this.state.count;
+    }
+}
+ 
+export default Counter;
---
+++ hatchling-web-app/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
+import "./styles.css"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+//ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
