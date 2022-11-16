221e1bf03905017a4dede4096e2322b925e187b4
Dagar-Rehan <112951301+Dagar-Rehan@users.noreply.github.com>
2022-11-16 17:31:19 -0600

    [TextEditor] Create editor component (#10)
    * Add text editor and run button.
    
    * Add line numbering to the editor.

---
+++ hatchling-web-app/src/components/TextEditor.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import '../TextEditor.css';
+
+class Texteditor extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+
+    render() {
+      return (
+        <div className="Home-container-div">
+            <form onSubmit={this.handleSubmit}>
+                <div className="Texteditor-div">
+                    <label>
+                        <textarea rows="10" cols="40" className="Texteditor" type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input className="Run-button" type="submit" value="Submit" />
+                </div>
+            </form>
+
+        </div>
+      );
+    }
+  }
+
+  export default Texteditor
