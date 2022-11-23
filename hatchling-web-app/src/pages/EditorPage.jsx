45811d759a884400ca425fcef7bb6f34400a75d6
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-11-23 10:55:32 -0600

    [pages] Add fade-in-out transistion to all pages

--- hatchling-web-app/src/pages/EditorPage.jsx
+++ hatchling-web-app/src/pages/EditorPage.jsx
@@ -1,12 +1,20 @@
+import { motion } from "framer-motion";
 import NavBar from "../components/NavBar";
 import TextEditor from "../components/TextEditor";
 
 export default function EditorPage() {
   return (
-    <div className="">
+    <>
       <NavBar />
-      <h1 className="text-center">Editor Page</h1>
-      <TextEditor />
-    </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 1.2 }}
+      >
+        <h1 className="text-center">Editor Page</h1>
+        <TextEditor />
+      </motion.div>
+    </>
   );
 }
--- hatchling-web-app/src/pages/HomePage.jsx
+++ hatchling-web-app/src/pages/HomePage.jsx
@@ -1,10 +1,18 @@
+import { motion } from "framer-motion";
 import NavBar from "../components/NavBar";
 
 export default function HomePage() {
   return (
-    <div className="">
+    <>
       <NavBar />
-      <h1 className="text-center">Home Page</h1>
-    </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 1.2 }}
+      >
+        <h1 className="text-center">Home Page</h1>
+      </motion.div>
+    </>
   );
 }
