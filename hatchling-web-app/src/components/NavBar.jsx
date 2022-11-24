c88372e54178adb3ce533f396200a5cbfb594a1e
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-11-23 18:19:37 -0600

    [NavBar] Add path highlighting

--- hatchling-web-app/src/components/NavBar.jsx
+++ hatchling-web-app/src/components/NavBar.jsx
@@ -1,9 +1,14 @@
 import logo from "../assets/Hatchling.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Navbar() {
   let [isOpen, setIsOpen] = useState(false);
+  let location = useLocation();
+
+  const paths = ["/", "/editor", "/characters", "/help", "/about"];
+
   return (
     <>
       <nav className="sticky top-0 z-10 grid grid-cols-2 grid-rows-1 bg-zinc-800 md:px-4 lg:px-8 xl:px-16">
@@ -20,31 +25,51 @@ export default function Navbar() {
           <div className="hidden md:flex gap-5 items-center px-4 md:px-8 bg-sky-900 h-fit border-2 rounded border-sky-900 ">
             <Link
               to="/"
-              className="text-white no-underline hover:scale-125 active:scale-90"
+              className={` ${
+                location.pathname.toLowerCase() === paths[0]
+                  ? "text-yellow-200 underline"
+                  : "text-white no-underline"
+              } hover:scale-125 active:scale-90`}
             >
               Home
             </Link>
             <Link
-              to="/Editor"
-              className="text-white no-underline hover:scale-125 active:scale-90"
+              to="/editor"
+              className={` ${
+                location.pathname.toLowerCase() === paths[1]
+                  ? "text-yellow-200 underline"
+                  : "text-white no-underline"
+              } hover:scale-125 active:scale-90`}
             >
               Editor
             </Link>
             <Link
-              to="/Characters"
-              className="text-white no-underline hover:scale-125 active:scale-90"
+              to="/characters"
+              className={` ${
+                location.pathname.toLowerCase() === paths[2]
+                  ? "text-yellow-200 underline"
+                  : "text-white no-underline"
+              } hover:scale-125 active:scale-90`}
             >
               Characters
             </Link>
             <Link
-              to="/Help"
-              className="text-white no-underline hover:scale-125 active:scale-90"
+              to="/help"
+              className={` ${
+                location.pathname.toLowerCase() === paths[3]
+                  ? "text-yellow-200 underline"
+                  : "text-white no-underline"
+              } hover:scale-125 active:scale-90`}
             >
               Help
             </Link>
             <Link
-              to="/About"
-              className="text-white no-underline hover:scale-125 active:scale-90"
+              to="/about"
+              className={` ${
+                location.pathname.toLowerCase() === paths[4]
+                  ? "text-yellow-200 underline"
+                  : "text-white no-underline"
+              } hover:scale-125 active:scale-90`}
             >
               About
             </Link>
@@ -61,44 +86,64 @@ export default function Navbar() {
           </div>
         </div>
       </nav>
-      <div
-        className={`w-screen bg-sky-900 text-white top-16 md:hidden ${
-          isOpen ? "sticky" : "hidden"
+      <motion.div
+        className={`w-screen bg-sky-900 text-white md:hidden z-20 ${
+          isOpen ? "fixed" : "hidden"
         }`}
       >
-        <div className="grid items-center justify-center text-center z-10 divide-y-2">
+        <div className="grid items-center justify-center text-center divide-y-2">
           <Link
             to="/"
-            className="text-white no-underline hover:scale-125 active:scale-90 py-4"
+            className={`${
+              location.pathname.toLowerCase() === paths[0]
+                ? "text-yellow-200"
+                : "text-white"
+            } no-underline hover:scale-125 active:scale-90 py-4`}
           >
             Home
           </Link>
           <Link
-            to="/Editor"
-            className="text-white no-underline hover:scale-125 active:scale-90 py-4"
+            to="/editor"
+            className={`${
+              location.pathname.toLowerCase() === paths[1]
+                ? "text-yellow-200"
+                : "text-white"
+            } no-underline hover:scale-125 active:scale-90 py-4`}
           >
             Editor
           </Link>
           <Link
-            to="/Characters"
-            className="text-white no-underline hover:scale-125 active:scale-90 py-4"
+            to="/characters"
+            className={`${
+              location.pathname.toLowerCase() === paths[2]
+                ? "text-yellow-200"
+                : "text-white"
+            } no-underline hover:scale-125 active:scale-90 py-4`}
           >
             Characters
           </Link>
           <Link
-            to="/Help"
-            className="text-white no-underline hover:scale-125 active:scale-90 py-4"
+            to="/help"
+            className={`${
+              location.pathname.toLowerCase() === paths[3]
+                ? "text-yellow-200"
+                : "text-white"
+            } no-underline hover:scale-125 active:scale-90 py-4`}
           >
             Help
           </Link>
           <Link
-            to="/About"
-            className="text-white no-underline hover:scale-125 active:scale-90 py-4"
+            to="/about"
+            className={`${
+              location.pathname.toLowerCase() === paths[4]
+                ? "text-yellow-200"
+                : "text-white"
+            } no-underline hover:scale-125 active:scale-90 py-4`}
           >
             About
           </Link>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
